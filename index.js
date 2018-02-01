2924bfae8a8e02d7f5e3ca1f78bbdee5f0b7a961
Jonathan <etaixjonathan@gmail.com>
2018-02-01 12:47:22 +0100

    use es5 syntax

--- index.js
+++ index.js
@@ -1,46 +1,42 @@
-'use strict';
+"use strict";
 
-
-var Through = require('through2');
-var MessageFormat = require('messageformat');
-var Gutil = require('gulp-util');
-var Yaml = require('js-yaml');
-var Marked = require('marked');
-
+var Through = require("through2");
+var MessageFormat = require("messageformat");
+var Gutil = require("gulp-util");
+var Yaml = require("js-yaml");
+var Marked = require("marked");
 
 module.exports = function (options) {
-
   var locales = {};
 
   var parse = function (file, encoding, next) {
-
-    var relativeParts = file.relative.split('/');
-    var ext = relativeParts[relativeParts.length - 1].split('.').pop();
+    var relativeParts = file.relative.split("/");
+    var ext = relativeParts[relativeParts.length - 1].split(".").pop();
     var message = {
       locale: relativeParts.shift(),
-      namespace: relativeParts.join('/').replace(/\.[^.]*$/, '').replace(/\\/g, '/'),
+      namespace: relativeParts
+        .join("/")
+        .replace(/\.[^.]*$/, "")
+        .replace(/\\/g, "/"),
       data: null
     };
 
     try {
-      if (ext === 'yml') {
+      if (ext === "yml") {
         message.data = Yaml.safeLoad(file.contents.toString());
-      }
-      else {
+      } else {
         message.data = JSON.parse(file.contents.toString());
       }
 
-      Object.keys(message.data || {}).forEach((key) => {
-
+      Object.keys(message.data || {}).forEach(function (key) {
         // Convert Markdown to HTML if string contains new line char.
-        if (message.data[key] && message.data[key].indexOf('\n') >= 0) {
+        if (message.data[key] && message.data[key].indexOf("\n") >= 0) {
           message.data[key] = Marked(message.data[key].trim(), {
             breaks: true
           });
         }
       });
-    }
-    catch (err) {
+    } catch (err) {
       console.error(err);
     }
 
@@ -51,17 +47,15 @@ module.exports = function (options) {
   };
 
   var flush = function (cb) {
-
-    Object.keys(locales).forEach((locale) => {
-
+    Object.keys(locales).forEach(function (locale) {
       var mf = new MessageFormat(locale);
       var data = locales[locale];
-      var compiled = 'window.i18n = (function () { ' + mf.compile(data) + ' })();';
-
-      this.push(new Gutil.File({
-        path: locale + '.js',
+      var compiled =
+        "window.i18n = (function () { " + mf.compile(data) + " })();";
+      return new Gutil.File({
+        path: locale + ".js",
         contents: new Buffer(compiled)
-      }));
+      });
     });
 
     cb();
